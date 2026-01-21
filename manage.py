062078c70ccf25d3f81b7b8d1d8ac824e71bdaee
neet1313 <prabhneet13@gmail.com>
2026-01-21 17:03:02 +0100

    refactor: Restart the project

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prabhneet.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'programming_exam.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
